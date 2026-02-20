19611de29799e42fa9b764ca9e43f45ad3cda8f6
Edwin Mwenda Mwiti <eduedwyn5@gmail.com>
2026-02-20 11:28:23 +0300

    Initial commit

---
+++ Work2/graph.m
@@ -0,0 +1,9 @@
+x = -1.5 : 0.01 : 1.5;
+y = x.^ 3 - x;
+figure;
+plot(x, y, 'b', 'LineWidth', 1.5);
+title('(a) Graph of y = x^3 - x on [-1.5, 1.5]');
+xlabel('x');
+ylabel('y');
+xlim([-1.5 1.5]);
+grid on;
---
+++ Work2/graph2.m
@@ -0,0 +1,15 @@
+x = -2 * pi : 0.01 : 2 * pi;
+y = tan(x);
+y(abs(y) > 10) = NaN;
+figure;
+plot(x, y, 'r', 'LineWidth', 1.5);
+title('(b) Graph of y = tan(x) on [-2\pi, 2\pi]');
+xlabel('x');
+ylabel('y');
+xlim([-2 * pi 2 * pi]);
+ylim([-10 10]);
+xticks(
+    [ -2 * pi, -3 * pi / 2, -pi, -pi / 2, 0, pi / 2, pi, 3 * pi / 2, 2 * pi ]);
+xticklabels({'-2\pi', '-3\pi/2', '-\pi', '-\pi/2', '0', '\pi/2', '\pi',
+             '3\pi/2', '2\pi'});
+grid on;
